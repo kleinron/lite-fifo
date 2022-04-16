6c402d73843f65363615d68da1c3fc106f8a0a5c
Ron Klein <solution@ronklein.co.il>
2022-04-16 10:49:32 +0300

    use env var for random seed if exists

--- test/src/setup.js
+++ test/src/setup.js
@@ -3,7 +3,7 @@
 const seedrandom = require("seedrandom");
 
 exports.mochaGlobalSetup = function() {
-    const now = new Date().toISOString();
+    const now = process.env['TEST_RANDOM_SEED'] || new Date().toISOString();
     console.log(`seed = ${now}`);
     seedrandom(now, { global: true });
 };
